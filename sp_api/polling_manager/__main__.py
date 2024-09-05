715d624e4bf772311e5935b92408fbe1928c81cf
mag1cfrog <harrywong2017@gmail.com>
2024-09-05 00:23:55 -0700

    feat: Add credentials and marketplace parameters to PollingManager constructor
    This commit adds the `credentials` and `marketplace` parameters to the `PollingManager` constructor in the `__main__.py` module. This allows passing credentials and marketplace information when creating an instance of `PollingManager`.

--- sp_api/polling_manager/__main__.py
+++ sp_api/polling_manager/__main__.py
@@ -1,8 +1,11 @@
 from loguru import logger
+from sp_api.base import Marketplaces
 
 class PollingManager:
-    def __init__(self, logging_configs=None):
+    def __init__(self, credentials: dict, marketplace: Marketplaces=Marketplaces.US, logging_configs=None):
         self.configure_logging(logging_configs)
+        self.credentials = credentials
+        self.marketplace = marketplace
 
     def configure_logging(self, configs):
         if configs is not None:
--- sp_api/polling_manager/orders.py
+++ sp_api/polling_manager/orders.py
@@ -1,33 +1,63 @@
+from loguru import logger
 from sp_api.util.backoff import exponential_backoff_with_jitter
+from sp_api.base import Marketplaces
+from sp_api.api import Orders
+
+def refresh_client(credentials: dict, marketplace: Marketplaces) -> Orders:
+    # Logic to refresh or create a new client
+    return Orders(credentials=credentials, marketplace=marketplace)
 
 
-def fetch_all_orders(orders_client, base_sleep_time: float=5, max_attempts: int=5, backoff_base: float=4, **kwargs):
+def fetch_all_orders(credentials: dict, marketplace: Marketplaces=Marketplaces.US, base_sleep_time: float=5, max_attempts: int=5, backoff_base: float=4, max_client_refreshes: int=3, **kwargs):
     all_orders = []
     next_token = None
     attempt = 1
-    
-    while True: # Handle pagination
+    should_continue = True
+    orders_client = Orders(credentials=credentials, marketplace=marketplace)
+    client_refresh = 0
+
+    while should_continue: 
         try:
             response = orders_client.get_orders(
                 NextToken=next_token,
                 **kwargs
             )
             all_orders.extend(response.payload['Orders'])
-            next_token = response.next_token
 
+            # Handle pagination
+            next_token = response.next_token 
             if not next_token:
-                break  # Exit loop if no more data to fetch
+                break 
+        
+        # Handle exceptions
         except Exception as e:
-            if orders_client.res.status_code == 429:  # Check if rate limit exceeded
+
+            # Handle rate limiting
+            if orders_client.res.status_code == 429:  
                 if attempt < max_attempts:
                     exponential_backoff_with_jitter(base_sleep_time, attempt, backoff_base)
                     attempt += 1
                     continue
                 else:
-                    raise Exception("API request rate exceeded after several retries")
+                    logger.error("API request rate exceeded after several retries")
+                    should_continue = False
+            
+            # Handle client expiration
+            if orders_client.res.status_code == 403 and "expired" in str(e).lower():
+                    if client_refresh < max_client_refreshes:
+                        logger.info("Refreshing client...")
+                        orders_client = refresh_client(credentials, marketplace)
+                        client_refresh += 1
+                        logger.info("Client refreshed, retrying...")
+                        continue
+                    else:
+                        logger.info("Client refresh limit exceeded")
+                        should_continue = False
+                        logger.error("Client refresh limit exceeded, stopping polling...")
+
             else:
-                print("An error occurred:", e)
-                break  # Break on other types of exceptions
+                # If unexpected error occurs, raise it immediately
+                raise Exception("An error occurred:", e)
 
 
     return all_orders
