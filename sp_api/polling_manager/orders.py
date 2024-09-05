92ff9ff1455f7a7aad7ef3765e443bbccc4cc237
mag1cfrog <harrywong2017@gmail.com>
2024-09-05 10:45:15 -0700

    feat: Add fetch_all_order_attributes function to orders.py
    This commit adds a new function `fetch_all_order_attributes` to the `orders.py` module in the `polling_manager` package. The function allows fetching all order attributes based on a given list of AmazonOrderIds. It utilizes the `fetch_attribute_func` and `refresh_client` functions to fetch order attributes and refresh the client if necessary. The function supports pagination, rate limiting, and retries using exponential backoff with jitter.

--- sp_api/polling_manager/orders.py
+++ sp_api/polling_manager/orders.py
@@ -1,6 +1,7 @@
-
+from typing import Callable
+from loguru import logger
 from sp_api.util.handle_api_error import handle_api_error
-from sp_api.base import Marketplaces
+from sp_api.base import Marketplaces, ApiResponse
 from sp_api.api import Orders
 
 def refresh_client(credentials: dict, marketplace: Marketplaces) -> Orders:
@@ -8,7 +9,46 @@ def refresh_client(credentials: dict, marketplace: Marketplaces) -> Orders:
     return Orders(credentials=credentials, marketplace=marketplace)
 
 
-def fetch_all_orders(credentials: dict, marketplace: Marketplaces=Marketplaces.US, base_sleep_time: float=5, max_attempts: int=5, backoff_base: float=4, max_client_refreshes: int=3, **kwargs):
+def fetch_all_orders(
+        credentials: dict, 
+        marketplace: Marketplaces=Marketplaces.US, 
+        base_sleep_time: float=5, 
+        max_attempts: int=5, 
+        backoff_base: float=4, 
+        max_client_refreshes: int=3, 
+        **kwargs
+    ) -> list:
+    """
+    Fetch all orders from the API.
+
+    Args:
+
+        credentials (dict): The credentials to use for the API client.
+        marketplace (Marketplaces): The marketplace to fetch orders from. Default is US.
+        base_sleep_time (float): The base sleep time in seconds. Default is 5.
+        max_attempts (int): The maximum number of attempts. Default is 5.
+        backoff_base (float): The base for exponential backoff. Default is 4.
+        max_client_refreshes (int): The maximum number of times the client can be refreshed. Default is 3.
+        **kwargs: Additional keyword arguments to pass to the API client.
+
+    Returns:
+        list: A list of orders.
+
+    Raises:
+        Exception: An unexpected error occurred.
+
+    Examples:
+        fetch_all_orders(
+            credentials=credentials,
+            marketplace=Marketplaces.US,
+            base_sleep_time=5,
+            max_attempts=5,
+            backoff_base=4,
+            max_client_refreshes=3
+        )
+
+
+    """
     all_orders = []
     next_token = None
     attempt = 1
@@ -49,3 +89,108 @@ def fetch_all_orders(credentials: dict, marketplace: Marketplaces=Marketplaces.U
 
 
     return all_orders
+
+
+def fetch_all_order_attributes(
+        orders: list, 
+        credentials: dict, 
+        fetch_attribute_func: Callable,
+        refresh_client: Callable,
+        marketplace: Marketplaces=Marketplaces.US, 
+        max_attempts: int=5,
+        max_client_refreshes: int=3,
+        base_sleep_time: float=5
+    )-> list:
+    """
+    Fetch all order attributes from the API based on a given list of AmazonOrderIds.
+
+    Args:
+    
+        orders (list): A list of AmazonOrderIds.
+        credentials (dict): The credentials to use for the API client.
+        fetch_attribute_func (Callable): The function to fetch order attributes.
+        refresh_client (Callable): The function to refresh the client.
+        marketplace (Marketplaces): The marketplace to fetch orders from.
+        max_attempts (int): The maximum number of attempts. Default is 5.
+        max_client_refreshes (int): The maximum number of times the client can be refreshed. Default is 3.
+        base_sleep_time (float): The base sleep time in seconds. Default is 5.
+
+    Returns:
+        list: A list of order attributes.
+
+    Raises:
+        Exception: An unexpected error occurred.
+
+    Examples:
+
+        fetch_all_order_attributes(
+            orders=orders,
+            credentials=credentials,
+            fetch_attribute_func=fetch_one_order_address,
+            refresh_client=refresh_client,
+            marketplace=Marketplaces.US,
+            max_attempts=5,
+            max_client_refreshes=3,
+            base_sleep_time=5
+
+        )
+
+    """
+    all_order_attributes = []
+    orders_client = Orders(credentials=credentials, marketplace=marketplace)
+    should_continue = True  # Flag to control the outer loop
+    
+    total_orders = len(orders)
+    order_count = 0
+    for order in orders:
+        next_token = None
+        
+        attempt = 1
+        
+        client_refresh = 0
+
+        while should_continue:
+            try:
+                response = fetch_attribute_func(order['AmazonOrderId'], orders_client, next_token)
+                print("Fetching order attribute for order:", order['AmazonOrderId'])
+                order_count += 1
+                all_order_attributes.append(response.payload)
+                next_token = response.next_token
+
+                if not next_token:
+                    break  # Exit loop if no more data to fetch
+            except Exception as e:
+                should_continue, client_refresh, orders_client = handle_api_error(
+                    e=e,
+                    api_client=orders_client,
+                    attempt=attempt,
+                    max_attempts=max_attempts,
+                    base_sleep_time=base_sleep_time,
+                    backoff_base=4,
+                    client_refresh=client_refresh,
+                    max_client_refreshes=max_client_refreshes,
+                    credentials=credentials,
+                    marketplace=marketplace,
+                    refresh_client=refresh_client
+                )
+
+        if not should_continue:
+            logger.error("Failed to fetch order attributes for order: {order_id}", order_id=order['AmazonOrderId'])
+            logger.error(f"{total_orders - order_count} orders left to fetch, {order_count/ total_orders}% finished ")
+            break  # Exit the outer loop if the flag is set to false
+        
+    print(f"Order fetching completed, total orders fetched: {order_count}")
+        
+    return all_order_attributes
+
+
+
+def fetch_one_order_address(order_id: str, order_client: Orders, next_token: str) -> ApiResponse:
+    """
+    Fetch one order address from the API.
+    """
+    return order_client.get_order_address(
+        order_id=order_id,
+        NextToken=next_token
+    )
+
